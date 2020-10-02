46a3be036f562c0478b895474f7d6a8b09228975
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-10-02 22:52:56 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,12 +1,9 @@
 import numpy as np
-#import pandas as pd
+import pandas as pd
 #import matplotlib.pyplot as plt
 import sklearn.datasets as sk
 
 def relu(x):
-    return x if x > 0 else 0
-
-def d_relu(x):
     return np.greater(x, 0).astype(int)
 
 def softmax(x):
@@ -17,22 +14,19 @@ def cross_entropy(x, y):
     a = 10 ** -8
     return -np.mean(y * np.log(x + (1 * a)))
 
-def create_weights(layers):
-    if not isinstance(layers, list): return
-    weights = []
-    for x, y in zip(layers, layers[1:]):
-        weights.append(np.random.rand(x, y))
-    return weights
 
-def 
+X, y = sk.make_classification(n_samples=1000, n_features=3, n_informative=3,  n_redundant=0, n_repeated=0
+                              , n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=3)
 
-x, y = sk.make_classification(n_samples=1000, n_features=3, n_informative=3,  n_redundant=0, n_repeated=0
-                              , n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=2)
+layers = []
+l = [3, 3, 4, 2] 
+l = [(l[x], l[x+1]) for x in range(len(l) - 1)]
 
-layers = [2, 3, 2]
-weights = create_weights(layers)
-x = np.array([[1,1], [2,2]])
-o = [x]
-for w in weights:
-    o.append(o[-1] @ w)
-print (o[-1])
+for c, x in enumerate(l):    
+    layers.append(np.random.rand(x[0], x[1]) * 5)
+
+o = [X[0]]
+for l in layers:
+    o.append(o[-1] @ l)
+    o[-1] = relu(o[-1]) * o[-1]
+print (o)
