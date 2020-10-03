42d20a84bcc41f3b60428d945a0795bd4b933c71
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-10-03 16:33:32 +0300

    commit to 'master'

--- nn.py
+++ nn.py
@@ -3,19 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.datasets as sk
 
-def create_layer(input, output):
-    return np.random.rand(input, output)
-
-def create_weights(layers):
-    weights = []
-    for x,y in zip(layers,layers[1:]):
-        weights.append([create_layer(x, y), np.random.rand(y)])
-    return weights
-
 def relu(x):
     return np.greater(x, 0).astype(int)
 
-def softmax(x):
+def softmax(z):
     assert len(z.shape) == 2
     s = np.max(z, axis=1)
     s = s[:, np.newaxis] # necessary step to do broadcasting
@@ -29,12 +20,8 @@ def cross_entropy(x, y):
     n = x.shape[0]
     return -np.sum(y * np.log(x + (1 * a))) / n
 
-
 X, y = sk.make_classification(n_samples=1000, n_features=2, n_informative=2,  n_redundant=0, n_repeated=0
                               , n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=3)
-
-plt.scatter(X[:, 0], X[:, 1], marker='o', c=y, edgecolor='k')
-plt.show()
 
 # creating network
 h_layers = []
@@ -46,15 +33,14 @@ for c, x in enumerate(l[:-1]):
 
 o_layer = np.random.rand(l[-1][0], l[-1][1])
 
-# 
+# forwards
 o = [X[:5]]
 for h in h_layers:
     o.append(o[-1] @ h)
     o[-1] = relu(o[-1]) * o[-1]
 o.append(o[-1] @ o_layer)
-for x in o[-1]:
-    print (x, '\n')
 o[-1] = softmax(o[-1])
-for x in o[-1]:
-    print (x, '\n')
 
+
+plt.scatter(X[:, 0], X[:, 1], marker='o', c=y, edgecolor='k')
+plt.show()
