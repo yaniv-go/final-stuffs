b391d589f88db65ec7f5da428d34956c81a6e1ea
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-10-04 18:59:58 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import sklearn.datasets as sk
 
 def relu(x):
@@ -82,14 +82,14 @@ def initalize_weights(l, code=0):
         p = np.sqrt(6 / (m + n))
         return np.random.uniform(-p, p, [m, n])
 
-def sgd(n_iter, X, h, b, e0=0.5, k=32, t=100):
+def sgd(n_iter, X, y, h, b, e0=0.5, k=32, t=100):
     depth = len(h) - 1
     data = pd.DataFrame()
     et = e0 / 100
     j = []
 
     for n in range(n_iter):
-        
+        print ('iteration: ', n, '\n\n')
         # forwards
         p = np.random.choice(X.shape[0], (k))
         X_t = np.array([X[i] for i in p])
@@ -99,33 +99,52 @@ def sgd(n_iter, X, h, b, e0=0.5, k=32, t=100):
         for l in range(depth):
             o.append(o[l] @ h[l] + b[l])
             o[l + 1] = o[l + 1] * relu(o[l + 1])
-        o.append(softmax(o[depth - 1] @ h[depth - 1]))
-        j.append(cross_entropy(o[depth], y_t))
+        o.append(softmax(o[depth] @ h[depth]))
+        
+        j.append(cross_entropy(o[depth + 1], y_t))
+        
         
         #backwards
         e = n/t ; e = (1 - e) * e0 + e * et 
         gradient = o[depth + 1] - y_t
+        print ('output of iteration: ', gradient, '\n\n')
         dw = []
+        db = []
+        # calculate gradients
         for l in range(depth, -1, -1):
             dw.append(o[l].T @ gradient / k)
             db.append(np.sum(gradient, axis=0) / k)
             gradient = gradient @ h[l].T ; gradient = relu(gradient) * gradient
+        
+        # use gradients
+        dw = dw[::-1]
+        db = db[::-1]
+        #print ('iteration gradient of output weights: ', dw[0], '\n')
+        #print ('iteration gradient of output bias: ', db[0], '\n\n\n')
+        for l in range(depth + 1):
+            h[l] = h[l] - dw[l] * e
+            b[l] = b[l] - db[l] * e
+        
+    return j
 
 
 
 
         
 
-X, y = sk.make_classification(n_samples=1000, n_features=2, n_informative=2,  n_redundant=0, n_repeated=0
-                              , n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=3)
+X, y = sk.make_classification(n_samples=1000, n_features=6, n_informative=5,  n_redundant=1, n_repeated=0
+                              , n_classes=3, n_clusters_per_class=2, flip_y=0.01, class_sep=2)
 
-layers = [2, 4, 4, 2]
-l_tuple(layers, 0)
+layers = [6, 8, 8, 4, 3] ; layers = l_tuple(layers, 0)
 hidden = []
 bias = [np.zeros(l[1]) + 0.1 for l in layers]
 for l in layers:
     hidden.append(initalize_weights(l))
 
-
-#sgd(1000, X, hidden, b)
-print(list(range(2 - 1, -1, -1)))
+for h in hidden:
+    print (h, '\n')
+print ('\n\n\nand now for the second time: \n')
+j = sgd(1000, X, y, hidden, bias, k=5) ; plt.scatter(range(len(j)), j) ; plt.show()
+for h in hidden:
+    print (h, '\n')
+#plt.scatter(X[:, 0], X[:, 1], marker='o', c=y, edgecolor='k') ; plt.show()
