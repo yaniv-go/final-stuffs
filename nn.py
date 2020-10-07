eb6cff029e9d40b81c7a0968c37cdf2431b7997c
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-10-07 13:13:28 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -3,7 +3,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.datasets as sk
 
+
 class MLP:
+    def __init__(self, layers, batchnorm=0):
+        assert isinstance(layers, list)
+        layers = self.l_tuple(layers, 0)
+        self.nn = {}
+        self.d = len(layers)
+
+        for i in range(self.d):
+            self.nn['W%d' % i] = np.random.rand(layers[i][0], layers[i][1]) * np.sqrt(2. / x)
+            self.nn['b%d' % i] = np.zeros(y)
+        if batchnorm == 1:
+            for x, y in layers:
+                self.nn['BN%d' % ]
+
+    def l_tuple(self, layers, i):
+        try:
+            layers[i] = (layers[i], layers[i + 1]) ; return(l_tuple(layers, i + 1))
+        except IndexError:
+            layers.pop(i) ; return layers
+
+     def get_batches(self, x, y, k):
+        p = np.random.permutation(len(x))
+        x, y = x[p], y[p]
+        y, p = [self.get_one_hot(i, self.h[self.d - 1].shape[1]) for i in y], len(x) // k
+
+        xb, yb = np.append(x, x[:k - (len(x) - p * k)], axis=0), np.append(np.array(y), y[:k - (len(y) - p * k)], axis=0)
+        return np.split(xb, p + 1), np.split(yb, p + 1)
+    
+    def get_learning_rate(self, e0, et, t, n):
+        if (k := n / t) < 1: return e0 * (1 - k) + et * t
+        return et
+
+    def relu(self, x):
+        return np.greater(x, 0).astype(int) * x
+
+    def d_relu(self, x):
+        return np.greater(x, 0).astype(int)
+
+    def softmax(self, z):
+        if not len(z.shape) == 2: z = np.array([z])
+        s = np.array([np.max(z,axis=1)]).T
+        e_z = np.exp(z - s)
+
+        return e_z / np.array([np.sum(e_z, axis=1)]).T
+
+    def cross_entropy(self, x, y):
+        a = 10 ** -8
+        n = x.shape[0]
+        return -np.sum(y * np.log(x + (1 * a))) / n
+
+    def get_one_hot(self, targets, nb_classes):
+        res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
+        return res.reshape(list(targets.shape)+[nb_classes])
+    
+    def weight_decay(self, h, wd):
+        if self.h == h: 
+            h = [np.power(l, 2) for l in h]
+            return np.sum([np.sum(l) for l in h]) * wd / 2
+        else: return wd * h
+
+class MLP1:
     def __init__(self, layers, a_foo, o_foo, reg, cost):
         assert isinstance(layers, list)
         self.layers = l_tuple(layers, 0)
@@ -197,8 +258,68 @@ class MLP:
                 g[i] = g[i] * (e / np.sqrt(1e-6 + r[i]))
                 v[i] = v[i] * m + g[i]
             for l in range(self.d):
-                self.h[l] -= v['W%d' % l] + e * (wd * self.h[l] + g['W%d' % l] )
-                self.b[l] -= v['b%d' % l] + e * g['b%d' % l]        
+                self.h[l] -= v['W%d' % l] + e * wd * self.h[l] + g['W%d' % l] 
+                self.b[l] -= v['b%d' % l] + g['b%d' % l]        
+            
+        return j
+
+    def adam(self, niter, x, y, e=0.01, wd=0.01, k=32, d1=0.9, d2=0.999):
+        xb, yb = self.get_batches(x, y, k) 
+        r = {} ; r_hat = {}
+        s = {} ; s_hat = {}
+        j = []
+
+        for i in range(self.d):
+            r['W%d' % i] = 0 ; r_hat['W%d' % i] = 0
+            r['b%d' % i] = 0 ; r_hat['b%d' % i] = 0
+            s['W%d' % i] = 0 ; s_hat['W%d' % i] = 0 
+            s['b%d' % i] = 0 ; s_hat['b%d' % i] = 0
+        
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt))
+            g = self.backprop(xt, yt, o, k, e)
+            for i in g.keys():
+                s[i] = d1*s[i] + (1 - d1) * g[i]
+                r[i] = d2*r[i] + (1 - d2) * g[i] * g[i]
+                s_hat[i] = s[i] / (1 - d1 ** (n+1))
+                r_hat[i] = r[i] / (1 - d2 ** (n+1))
+            for l in range(self.d):
+                self.h[l] -= e * (s_hat['W%d' % l] / np.sqrt(r_hat['W%d' % l] + 1e-8) + wd * self.h[l])
+                self.b[l] -= e * (s_hat['b%d' % l] / np.sqrt(r_hat['b%d' % l] + 1e-8))
+        
+        return j
+
+    def adam_with_momentum(self, niter, x, y, e=0.01, wd=0.01, k=32, m=0.9, d=0.999):
+        xb, yb = self.get_batches(x, y, k)
+        r = {} ; r_hat = {}
+        v = {} ; v_hat = {}
+        j = []
+
+        for i in range(self.d):
+            r['W%d' % i] = 0 ; r_hat['W%d' % i] = 0
+            r['b%d' % i] = 0 ; r_hat['b%d' % i] = 0
+            v['W%d' % i] = 0 ; v_hat['W%d' % i] = 0 
+            v['b%d' % i] = 0 ; v_hat['b%d' % i] = 0
+
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt))
+            g = self.backprop(xt,yt, o, k, e)
+            for i in g.keys():
+                v[i] = m * v[i] + (1-m) * g[i]
+                r[i] = d * r[i] + (1-d) * g[i] * g[i]
+                v_hat[i] = ((m * v[i]) / (1 - m ** (n + 1))) + (((1 - m) * g[i]) / (1 - m ** (n + 1))) 
+                r_hat[i] = (d * r[i]) / (1 - d ** (n + 1))
+            for l in range(self.d):
+                self.h[l] -= e * (v_hat['W%d' % l] * (1 / np.sqrt(r_hat['W%d' % l] + 1e-8)) + wd * self.h[l])
+                self.b[l] -= e * (v_hat['b%d' % l] * (1 / np.sqrt(r_hat['b%d' % l] + 1e-8)))
             
         return j
 
@@ -255,10 +376,10 @@ def l_tuple(layers, i):
     except IndexError:
         layers.pop(i) ; return layers
 
-nn = MLP([20, 22, 20, 16, 10 ,2], 'r', 'so', 'l2', 'co')
-x, y = sk.make_classification(n_samples=100, n_features=20, n_informative=2, n_redundant=2
+nn = MLP1([20, 22, 20, 16, 10 ,2], 'r', 'so', 'l2', 'co')
+x, y = sk.make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=2
                            , n_repeated=0, n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=1.0)
 
-j = nn.rmsprop_with_momentum(2000, x, y, d=0.9, e=0.001, wd=1e-2)
+j = nn.adam_with_momentum(1000, x, y, e=1e-2, wd=0.01)
 plt.plot(range(len(j)), j)
 plt.show() 
