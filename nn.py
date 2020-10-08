8da2c90ef16f4ce82e378a0ab7cd23486e29d542
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-10-08 15:59:27 +0300

    a

--- nn.py
+++ nn.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.datasets as sk
-
+import random
 
 class MLP:
     def __init__(self, layers, batchnorm=0):
@@ -33,7 +33,7 @@ class MLP:
         for l in range(self.d - 1, -1, -1):
             g['W%d' % l] = (o[l].T @ de) / k
             g['b%d' % l] = np.sum(de, axis=0) / k
-            de = de @ g['W%d' % l].T
+            de = de @ self.nn['W%d' % l].T
         
         return g
 
@@ -120,6 +120,7 @@ class MLP:
 
         for ep in range(epochs):
             e = self.get_learning_rate(e0, et, t, ep)
+            xb, yb = np.random.Generator.permutation(xb), np.random.Generator.permutation(yb)
             for xt, yt in zip(xb, yb):
                 o = self.forward(xt)
                 j.append(self.cost(o[self.d], yt))
@@ -169,324 +170,48 @@ class MLP:
                 o = self.forward(xt)
                 jv.append(self.cost(o[self.d], yt))
         return j, jv       
-
-class MLP1:
-    def __init__(self, layers, a_foo, o_foo, reg, cost):
-        assert isinstance(layers, list)
-        self.layers = l_tuple(layers, 0)
-        self.logits = {'r':self.relu, 's':self.sigmoid, 'so':self.softmax}
-        self.regulizeres = {'l2': self.weight_decay}
-        self.costs = {'co':self.cross_entropy}
-        self.d = len(self.layers)
-        self.h = []
-        self.b = []
-
-        if not a_foo in self.logits: self.f = self.relu
-        else: self.f = self.logits[a_foo]
-        if not o_foo in self.logits: self.f_o = self.softmax
-        else: self.f_o = self.logits[o_foo] 
-        if not reg in self.regulizeres: self.reg = self.weight_decay
-        else: self.reg = self.regulizeres[reg]
-        if not cost in self.costs: self.cost = self.cross_entropy
-        else: self.cost = self.costs[cost]
-
-        if self.f == self.relu:
-            for l in self.layers:
-                self.h.append(np.random.rand(l[0], l[1]) * np.sqrt(2./l[0]))
-                self.b.append(np.zeros(l[1]))
-        else: 
-            for l in self.layers:
-                self.h.append(np.random.rand(l[0], l[1]) * np.sqrt(6/(l[0] + l[1])))
-                self.b.append(np.zeros(l[1]))
-
-    def feed_forward(self, x):
-        o = [x]
-        for l in range(self.d - 1):
-            o.append(o[l] @ self.h[l] + self.b[l])
-            o[l + 1] = self.f(o[l + 1]) 
-        o.append(o[self.d - 1] @ self.h[self.d - 1] + self.b[self.d - 1])
-        o[self.d] = self.f_o(o[self.d])
-
-        return o
     
-    def backprop(self, x, y, o, k, e):
-        g = {}
-        de = o[self.d] - y
-
-        for l in range(self.d - 1, -1, -1):
-            g['W%d' % l] = (o[l].T @ de) / k
-            g['b%d' % l] = np.sum(de, axis=0) / k
-            de = de @ self.h[l].T
-        
-        return g
-
-    def gradient_descent(self, niter, x, y, e, wd=0.1):
-        j = []
-        for n in range(niter):
-            p = np.random.choice(x.shape[0])
-            x_T, y_T = x[p], self.get_one_hot(y[p], self.h[self.d - 1].shape[1])
-
-            o = self.feed_forward(x_T)
-            j.append(self.cost(o[self.d], y_T) + self.weight_decay(self.h, wd))
-            o = [np.array([i]) if len(i.shape) < 2 else i for i in o]
-
-            de = o[self.d] - y_T
-            for l in range(self.d-1, -1, -1):                
-                dw = o[l].T @ de
-                db = de.flatten()
-                de = de @ self.h[l].T
-                self.h[l] -= e * (dw + wd * self.h[l])
-                self.b[l] -= db * e
-            
-        return j
-    
-    def sgd(self, niter, x, y, e0=0.01, t=500, et=0, wd=0.01, k=32):
-        if et == 0: et = e0 / 100
+    def rmsprop(self, epochs, x, y, e=0.01, wd=0.01, k=32, d=0.9):
         xb, yb = self.get_batches(x, y, k)
-        j = []        
-        
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
-
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt) + self.weight_decay(self.h, wd))
-
-            e = self.get_learning_rate(e0, et, t, n)
-            g = self.backprop(xt, yt, o, k, e)
-            for l in range(self.d):
-                self.h[l] -= e * (g['W%d' % l] + wd * self.h[l])
-                self.b[l] -= e * g['b%d' % l]
-        
-        return j
-
-    def sgd_with_momentum(self, niter, x, y, e0=0.01, et=0, t=500, wd=0.01, k=32, m=0.9):
-        if et == 0: et = e0 / 100
-        xb, yb = self.get_batches(x, y, k)
-        v = {}
-        j = []
-
-        for i in range(self.d):
-            v['W%d' % i] = 0
-            v['b%d' % i] = 0
-
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
-
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt) + self.weight_decay(self.h, wd))
-
-            e = self.get_learning_rate(e0, et, t, n)
-            g = self.backprop(xt, yt, o, k, e)
-            for l in range(self.d):
-                self.h[l] -= v['W%d' % l] + wd * e * self.h[l]
-                self.b[l] -= v['b%d' % l] 
-            for i in g.keys(): 
-                v[i] = m * v[i] + e * g[i]
-
-        return j
-
-    def sgd_with_nesterov_momentum(self, niter, x, y, e0=0.01, et=0, t=500, wd=0.01, k=32, m=0.5):
-        if et == 0: et = e0 / 100
-        xb, yb = self.get_batches(x, y, k) 
-        v = {}
-        j = []
-
-        for i in range(self.d):
-            v['W%d' % i] = 0
-            v['b%d' % i] = 0
-
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
-
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt) + self.weight_decay(self.h, wd))
-
-            e = self.get_learning_rate(e0, et, t, n)
-            g = self.backprop(xt, yt, o, k, e)
-            for i in g.keys(): 
-                v[i] = m * v[i] + e * g[i]      
-            for l in range(self.d):
-                self.h[l] -= v['W%d' % l] + e * (wd * self.h[l] + g['W%d' % l] )
-                self.b[l] -= v['b%d' % l] + e * g['b%d' % l]        
-        
-        return j
-
-    def rmsprop(self, niter, x, y, e=0.01, wd=0.01, k=32, d=0.9):
-        xb, yb = self.get_batches(x, y, k) 
+        xv, yv = [], []
+        j, jv = [], []
         r = {}
-        j = []
+        for i in self.nn.keys():
+            r[i] = 0
 
-        for i in range(self.d):
-            r['W%d' % i] = 0
-            r['b%d' % i] = 0
+        if ((p := len(xb) // 5) >= 1):
+            for i in range(p):
+                xv.append(xb.pop()) ; yv.append(yb.pop())
+        else: xv = xb.pop() ; yv = yb.pop()
 
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
+        for ep in range(epochs):
+            random.shuffle(xb) ; random.shuffle(yb)
+            for xt, yt in zip(xb, yb):
+                o = self.forward(xt)
+                j.append(self.cost(o[self.d], yt))
 
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt))
-
-            g = self.backprop(xt, yt, o, k, e)
-            for i in g.keys():
-                r[i] = d * r[i] + (1 - d) * g[i] * g[i]
-                g[i] = g[i] * (1 / np.sqrt(1e-6 + r[i])) * e
-            for l in range(self.d):
-                self.h[l] -= g['W%d' % l] + e * wd * self.h[l]
-                self.b[l] -= g['b%d' % l] 
-
-        return j
-
-    def rmsprop_with_momentum(self, niter, x, y, e=0.001, wd=0.01, k=32, m=0.9, d=0.9):
-        xb, yb = self.get_batches(x, y, k) 
-        r = {}
-        v = {}
-        j = []
-
-        for i in range(self.d):
-            r['W%d' % i] = 0
-            r['b%d' % i] = 0
-            v['W%d' % i] = 0
-            v['b%d' % i] = 0
-        
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
-
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt))
-            g = self.backprop(xt, yt, o, k, e)
-            for i in g.keys():
-                r[i] = r[i] * d + (1 - d) * g[i] * g[i]
-                g[i] = g[i] * (e / np.sqrt(1e-6 + r[i]))
-                v[i] = v[i] * m + g[i]
-            for l in range(self.d):
-                self.h[l] -= v['W%d' % l] + e * wd * self.h[l] + g['W%d' % l] 
-                self.b[l] -= v['b%d' % l] + g['b%d' % l]        
-            
-        return j
-
-    def adam(self, niter, x, y, e=0.01, wd=0.01, k=32, d1=0.9, d2=0.999):
-        xb, yb = self.get_batches(x, y, k) 
-        r = {} ; r_hat = {}
-        s = {} ; s_hat = {}
-        j = []
-
-        for i in range(self.d):
-            r['W%d' % i] = 0 ; r_hat['W%d' % i] = 0
-            r['b%d' % i] = 0 ; r_hat['b%d' % i] = 0
-            s['W%d' % i] = 0 ; s_hat['W%d' % i] = 0 
-            s['b%d' % i] = 0 ; s_hat['b%d' % i] = 0
-        
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
-
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt))
-            g = self.backprop(xt, yt, o, k, e)
-            for i in g.keys():
-                s[i] = d1*s[i] + (1 - d1) * g[i]
-                r[i] = d2*r[i] + (1 - d2) * g[i] * g[i]
-                s_hat[i] = s[i] / (1 - d1 ** (n+1))
-                r_hat[i] = r[i] / (1 - d2 ** (n+1))
-            for l in range(self.d):
-                self.h[l] -= e * (s_hat['W%d' % l] / np.sqrt(r_hat['W%d' % l] + 1e-8) + wd * self.h[l])
-                self.b[l] -= e * (s_hat['b%d' % l] / np.sqrt(r_hat['b%d' % l] + 1e-8))
-        
-        return j
-
-    def adam_with_momentum(self, niter, x, y, e=0.01, wd=0.01, k=32, m=0.9, d=0.999):
-        xb, yb = self.get_batches(x, y, k)
-        r = {} ; r_hat = {}
-        v = {} ; v_hat = {}
-        j = []
-
-        for i in range(self.d):
-            r['W%d' % i] = 0 ; r_hat['W%d' % i] = 0
-            r['b%d' % i] = 0 ; r_hat['b%d' % i] = 0
-            v['W%d' % i] = 0 ; v_hat['W%d' % i] = 0 
-            v['b%d' % i] = 0 ; v_hat['b%d' % i] = 0
-
-        for n in range(niter):
-            p = np.random.choice(len(xb))
-            xt, yt = xb[p], yb[p]
-
-            o = self.feed_forward(xt)
-            j.append(self.cost(o[self.d], yt))
-            g = self.backprop(xt,yt, o, k, e)
-            for i in g.keys():
-                v[i] = m * v[i] + (1-m) * g[i]
-                r[i] = d * r[i] + (1-d) * g[i] * g[i]
-                v_hat[i] = ((m * v[i]) / (1 - m ** (n + 1))) + (((1 - m) * g[i]) / (1 - m ** (n + 1))) 
-                r_hat[i] = (d * r[i]) / (1 - d ** (n + 1))
-            for l in range(self.d):
-                self.h[l] -= e * (v_hat['W%d' % l] * (1 / np.sqrt(r_hat['W%d' % l] + 1e-8)) + wd * self.h[l])
-                self.b[l] -= e * (v_hat['b%d' % l] * (1 / np.sqrt(r_hat['b%d' % l] + 1e-8)))
-            
-        return j
-
-    def get_batches(self, x, y, k):
-        p = np.random.permutation(len(x))
-        x, y = x[p], y[p]
-        y, p = [self.get_one_hot(i, self.h[self.d - 1].shape[1]) for i in y], len(x) // k
-
-        xb, yb = np.append(x, x[:k - (len(x) - p * k)], axis=0), np.append(np.array(y), y[:k - (len(y) - p * k)], axis=0)
-        return np.split(xb, p + 1), np.split(yb, p + 1)
-    
-    def get_learning_rate(self, e0, et, t, n):
-        if (k := n / t) < 1: return e0 * (1 - k) + et * t
-        return et
-
-    def relu(self, x):
-        return np.greater(x, 0).astype(int) * x
-
-    def d_relu(self, x):
-        return np.greater(x, 0).astype(int)
-
-    def sigmoid(self, x):  
-        return np.exp(-np.logaddexp(0, -x))
-
-    def d_sigmoid(self, x):
-        y = self.sigmoid(x)
-        return y * (1 - y)
-
-    def softmax(self, z):
-        if not len(z.shape) == 2: z = np.array([z])
-        s = np.array([np.max(z,axis=1)]).T
-        e_z = np.exp(z - s)
-
-        return e_z / np.array([np.sum(e_z, axis=1)]).T
-
-    def cross_entropy(self, x, y):
-        a = 10 ** -8
-        n = x.shape[0]
-        return -np.sum(y * np.log(x + (1 * a))) / n
-
-    def get_one_hot(self, targets, nb_classes):
-        res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
-        return res.reshape(list(targets.shape)+[nb_classes])
-    
-    def weight_decay(self, h, wd):
-        if self.h == h: 
-            h = [np.power(l, 2) for l in h]
-            return np.sum([np.sum(l) for l in h]) * wd / 2
-        else: return wd * h
+                g = self.backprop(xt, yt, o, k)
+                for i in g.keys():
+                    r[i] = d * r[i] + (1 - d) * g[i] * g[i]
+                    g[i] = e * (1 / np.sqrt(1e-8 + r[i])) * g[i]
+                for l in range(self.d):
+                    self.nn['W%d' % l] -= e * (wd * self.nn['W%d' % l]) + g['W%d' % l]
+                    self.nn['b%d' % l] -= g['b%d' % l]
+            for xt, yt in zip(xv, yv):
+                o = self.forward(xt)
+                jv.append(self.cost(o[self.d], yt))
+        return j, jv
 
 def l_tuple(layers, i):
     try:
         layers[i] = (layers[i], layers[i + 1]) ; return(l_tuple(layers, i + 1))
     except IndexError:
         layers.pop(i) ; return layers
-
 nn = MLP([20, 22, 20, 16, 10 ,2])
 x, y = sk.make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=2
                            , n_repeated=0, n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=1.0)
 
-j, jv = nn.sgd_momentum(100, x, y)
+j, jv = nn.rmsprop(100, x, y, e=1e-2, wd=0)
 fig, axs = plt.subplots(2)
 axs[0].plot(range(len(j)), j)
 axs[1].plot(range(len(jv)), jv)
--- nn_again.py
+++ nn_again.py
@@ -3,24 +3,312 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.datasets as sk
 
-def relu(x):
-    return np.greater(x, 0).astype(int)
 
-def softmax(z):
-    assert len(z.shape) == 2
-    s = np.array([np.max(z,axis=1)]).T
-    e_z = np.exp(z - s)
+class MLP1:
+    def __init__(self, layers, a_foo=0, o_foo=0, reg=0, cost=0):
+        assert isinstance(layers, list)
+        self.layers = l_tuple(layers, 0)
+        self.logits = {'r':self.relu, 's':self.sigmoid, 'so':self.softmax}
+        self.regulizeres = {'l2': self.weight_decay}
+        self.costs = {'co':self.cross_entropy}
+        self.d = len(self.layers)
+        self.h = []
+        self.b = []
 
-    return e_z / np.array([np.sum(e_z, axis=1)]).T
+        if not a_foo in self.logits: self.f = self.relu
+        else: self.f = self.logits[a_foo]
+        if not o_foo in self.logits: self.f_o = self.softmax
+        else: self.f_o = self.logits[o_foo] 
+        if not reg in self.regulizeres: self.reg = self.weight_decay
+        else: self.reg = self.regulizeres[reg]
+        if not cost in self.costs: self.cost = self.cross_entropy
+        else: self.cost = self.costs[cost]
 
-def cross_entropy(x, y):
-    a = 10 ** -8
-    n = x.shape[0]
-    return -np.sum(y * np.log(x + (1 * a))) / n
+        if self.f == self.relu:
+            for l in self.layers:
+                self.h.append(np.random.rand(l[0], l[1]) * np.sqrt(2./l[0]))
+                self.b.append(np.zeros(l[1]))
+        else: 
+            for l in self.layers:
+                self.h.append(np.random.rand(l[0], l[1]) * np.sqrt(6/(l[0] + l[1])))
+                self.b.append(np.zeros(l[1]))
 
-def get_one_hot(targets, nb_classes):
-    res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
-    return res.reshape(list(targets.shape)+[nb_classes])
+    def feed_forward(self, x):
+        o = [x]
+        for l in range(self.d - 1):
+            o.append(o[l] @ self.h[l] + self.b[l])
+            o[l + 1] = self.f(o[l + 1]) 
+        o.append(o[self.d - 1] @ self.h[self.d - 1] + self.b[self.d - 1])
+        o[self.d] = self.f_o(o[self.d])
+
+        return o
+    
+    def backprop(self, x, y, o, k, e):
+        g = {}
+        de = o[self.d] - y
+
+        for l in range(self.d - 1, -1, -1):
+            g['W%d' % l] = (o[l].T @ de) / k
+            g['b%d' % l] = np.sum(de, axis=0) / k
+            de = de @ self.h[l].T
+        
+        return g
+
+    def gradient_descent(self, niter, x, y, e, wd=0.1):
+        j = []
+        for n in range(niter):
+            p = np.random.choice(x.shape[0])
+            x_T, y_T = x[p], self.get_one_hot(y[p], self.h[self.d - 1].shape[1])
+
+            o = self.feed_forward(x_T)
+            j.append(self.cost(o[self.d], y_T) + self.weight_decay(self.h, wd))
+            o = [np.array([i]) if len(i.shape) < 2 else i for i in o]
+
+            de = o[self.d] - y_T
+            for l in range(self.d-1, -1, -1):                
+                dw = o[l].T @ de
+                db = de.flatten()
+                de = de @ self.h[l].T
+                self.h[l] -= e * (dw + wd * self.h[l])
+                self.b[l] -= db * e
+            
+        return j
+    
+    def sgd(self, niter, x, y, e0=0.01, t=500, et=0, wd=0.01, k=32):
+        if et == 0: et = e0 / 100
+        xb, yb = self.get_batches(x, y, k)
+        j = []        
+        
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt) + self.weight_decay(self.h, wd))
+
+            e = self.get_learning_rate(e0, et, t, n)
+            g = self.backprop(xt, yt, o, k, e)
+            for l in range(self.d):
+                self.h[l] -= e * (g['W%d' % l] + wd * self.h[l])
+                self.b[l] -= e * g['b%d' % l]
+        
+        return j
+
+    def sgd_with_momentum(self, niter, x, y, e0=0.01, et=0, t=500, wd=0.01, k=32, m=0.9):
+        if et == 0: et = e0 / 100
+        xb, yb = self.get_batches(x, y, k)
+        v = {}
+        j = []
+
+        for i in range(self.d):
+            v['W%d' % i] = 0
+            v['b%d' % i] = 0
+
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt) + self.weight_decay(self.h, wd))
+
+            e = self.get_learning_rate(e0, et, t, n)
+            g = self.backprop(xt, yt, o, k, e)
+            for l in range(self.d):
+                self.h[l] -= v['W%d' % l] + wd * e * self.h[l]
+                self.b[l] -= v['b%d' % l] 
+            for i in g.keys(): 
+                v[i] = m * v[i] + e * g[i]
+
+        return j
+
+    def sgd_with_nesterov_momentum(self, niter, x, y, e0=0.01, et=0, t=500, wd=0.01, k=32, m=0.5):
+        if et == 0: et = e0 / 100
+        xb, yb = self.get_batches(x, y, k) 
+        v = {}
+        j = []
+
+        for i in range(self.d):
+            v['W%d' % i] = 0
+            v['b%d' % i] = 0
+
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt) + self.weight_decay(self.h, wd))
+
+            e = self.get_learning_rate(e0, et, t, n)
+            g = self.backprop(xt, yt, o, k, e)
+            for i in g.keys(): 
+                v[i] = m * v[i] + e * g[i]      
+            for l in range(self.d):
+                self.h[l] -= v['W%d' % l] + e * (wd * self.h[l] + g['W%d' % l] )
+                self.b[l] -= v['b%d' % l] + e * g['b%d' % l]        
+        
+        return j
+
+    def rmsprop(self, niter, x, y, e=0.01, wd=0.01, k=32, d=0.9):
+        xb, yb = self.get_batches(x, y, k) 
+        r = {}
+        j = []
+
+        for i in range(self.d):
+            r['W%d' % i] = 0
+            r['b%d' % i] = 0
+
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt))
+
+            g = self.backprop(xt, yt, o, k, e)
+            for i in g.keys():
+                r[i] = d * r[i] + (1 - d) * g[i] * g[i]
+                g[i] = g[i] * (1 / np.sqrt(1e-6 + r[i])) * e
+            for l in range(self.d):
+                self.h[l] -= g['W%d' % l] + e * wd * self.h[l]
+                self.b[l] -= g['b%d' % l] 
+
+        return j
+
+    def rmsprop_with_momentum(self, niter, x, y, e=0.001, wd=0.01, k=32, m=0.9, d=0.9):
+        xb, yb = self.get_batches(x, y, k) 
+        r = {}
+        v = {}
+        j = []
+
+        for i in range(self.d):
+            r['W%d' % i] = 0
+            r['b%d' % i] = 0
+            v['W%d' % i] = 0
+            v['b%d' % i] = 0
+        
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt))
+            g = self.backprop(xt, yt, o, k, e)
+            for i in g.keys():
+                r[i] = r[i] * d + (1 - d) * g[i] * g[i]
+                g[i] = g[i] * (e / np.sqrt(1e-6 + r[i]))
+                v[i] = v[i] * m + g[i]
+            for l in range(self.d):
+                self.h[l] -= v['W%d' % l] + e * wd * self.h[l] + g['W%d' % l] 
+                self.b[l] -= v['b%d' % l] + g['b%d' % l]        
+            
+        return j
+
+    def adam(self, niter, x, y, e=0.01, wd=0.01, k=32, d1=0.9, d2=0.999):
+        xb, yb = self.get_batches(x, y, k) 
+        r = {} ; r_hat = {}
+        s = {} ; s_hat = {}
+        j = []
+
+        for i in range(self.d):
+            r['W%d' % i] = 0 ; r_hat['W%d' % i] = 0
+            r['b%d' % i] = 0 ; r_hat['b%d' % i] = 0
+            s['W%d' % i] = 0 ; s_hat['W%d' % i] = 0 
+            s['b%d' % i] = 0 ; s_hat['b%d' % i] = 0
+        
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt))
+            g = self.backprop(xt, yt, o, k, e)
+            for i in g.keys():
+                s[i] = d1*s[i] + (1 - d1) * g[i]
+                r[i] = d2*r[i] + (1 - d2) * g[i] * g[i]
+                s_hat[i] = s[i] / (1 - d1 ** (n+1))
+                r_hat[i] = r[i] / (1 - d2 ** (n+1))
+            for l in range(self.d):
+                self.h[l] -= e * (s_hat['W%d' % l] / np.sqrt(r_hat['W%d' % l] + 1e-8) + wd * self.h[l])
+                self.b[l] -= e * (s_hat['b%d' % l] / np.sqrt(r_hat['b%d' % l] + 1e-8))
+        
+        return j
+
+    def adam_with_momentum(self, niter, x, y, e=0.01, wd=0.01, k=32, m=0.9, d=0.999):
+        xb, yb = self.get_batches(x, y, k)
+        r = {} ; r_hat = {}
+        v = {} ; v_hat = {}
+        j = []
+
+        for i in range(self.d):
+            r['W%d' % i] = 0 ; r_hat['W%d' % i] = 0
+            r['b%d' % i] = 0 ; r_hat['b%d' % i] = 0
+            v['W%d' % i] = 0 ; v_hat['W%d' % i] = 0 
+            v['b%d' % i] = 0 ; v_hat['b%d' % i] = 0
+
+        for n in range(niter):
+            p = np.random.choice(len(xb))
+            xt, yt = xb[p], yb[p]
+
+            o = self.feed_forward(xt)
+            j.append(self.cost(o[self.d], yt))
+            g = self.backprop(xt,yt, o, k, e)
+            for i in g.keys():
+                v[i] = m * v[i] + (1-m) * g[i]
+                r[i] = d * r[i] + (1-d) * g[i] * g[i]
+                v_hat[i] = ((m * v[i]) / (1 - m ** (n + 1))) + (((1 - m) * g[i]) / (1 - m ** (n + 1))) 
+                r_hat[i] = (d * r[i]) / (1 - d ** (n + 1))
+            for l in range(self.d):
+                self.h[l] -= e * (v_hat['W%d' % l] * (1 / np.sqrt(r_hat['W%d' % l] + 1e-8)) + wd * self.h[l])
+                self.b[l] -= e * (v_hat['b%d' % l] * (1 / np.sqrt(r_hat['b%d' % l] + 1e-8)))
+            
+        return j
+
+    def get_batches(self, x, y, k):
+        p = np.random.permutation(len(x))
+        x, y = x[p], y[p]
+        y, p = [self.get_one_hot(i, self.h[self.d - 1].shape[1]) for i in y], len(x) // k
+
+        xb, yb = np.append(x, x[:k - (len(x) - p * k)], axis=0), np.append(np.array(y), y[:k - (len(y) - p * k)], axis=0)
+        return np.split(xb, p + 1), np.split(yb, p + 1)
+    
+    def get_learning_rate(self, e0, et, t, n):
+        if (k := n / t) < 1: return e0 * (1 - k) + et * t
+        return et
+
+    def relu(self, x):
+        return np.greater(x, 0).astype(int) * x
+
+    def d_relu(self, x):
+        return np.greater(x, 0).astype(int)
+
+    def sigmoid(self, x):  
+        return np.exp(-np.logaddexp(0, -x))
+
+    def d_sigmoid(self, x):
+        y = self.sigmoid(x)
+        return y * (1 - y)
+
+    def softmax(self, z):
+        if not len(z.shape) == 2: z = np.array([z])
+        s = np.array([np.max(z,axis=1)]).T
+        e_z = np.exp(z - s)
+
+        return e_z / np.array([np.sum(e_z, axis=1)]).T
+
+    def cross_entropy(self, x, y):
+        a = 10 ** -8
+        n = x.shape[0]
+        return -np.sum(y * np.log(x + (1 * a))) / n
+
+    def get_one_hot(self, targets, nb_classes):
+        res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
+        return res.reshape(list(targets.shape)+[nb_classes])
+    
+    def weight_decay(self, h, wd):
+        if self.h == h: 
+            h = [np.power(l, 2) for l in h]
+            return np.sum([np.sum(l) for l in h]) * wd / 2
+        else: return wd * h
 
 def l_tuple(layers, i):
     try:
@@ -28,52 +316,11 @@ def l_tuple(layers, i):
     except IndexError:
         layers.pop(i) ; return layers
 
+nn = MLP1([20, 22, 20, 16, 10 ,2])
+x, y = sk.make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=2
+                           , n_repeated=0, n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=1.0)
 
-X, y = sk.make_classification(n_samples=1000, n_features=4, n_informative=4,  n_redundant=0, n_repeated=0
-                              , n_classes=2, n_clusters_per_class=2, flip_y=0.01, class_sep=2)
-
-
-layers = [4, 4, 3, 2] ; layers = l_tuple(layers, 0)
-hidden = []
-bias = [np.zeros(l[1]) for l in layers]
-e0 = 0.01
-et = 0.0000001
-t = 1000
-for l in layers:
-    hidden.append(np.random.rand(l[0], l[1]) * np.sqrt(2./l[0]))
-
-j = [] ; d = len(hidden)
-wd = 0.05
-
-for n in range(3000):
-    r = np.random.choice(X.shape[0], 1)
-    o = [X[r]]
-    
-    #forwards
-    for i in range(d - 1):
-        o.append(o[-1] @ hidden[i] + bias[i])
-        o[-1] = relu(o[-1]) * o[-1]
-    o.append(softmax(o[-1] @ hidden[d - 1]))    
-
-    #error
-    w = [np.power(h, 2) for h in hidden]
-    w = np.sum([np.sum(i) for i in w])
-    j.append(cross_entropy(o[-1], get_one_hot(y[r], 2)))
-
-    # backwards
-    e = e0 * (1 - n/t) + et
-    de = o[-1] - get_one_hot(y[r], 2)
-    for i in range(d-1, -1, -1):
-        dw = o[i].T @ de
-        db = de.flatten()
-        de = de @ hidden[i].T
-        hidden[i] -= (dw + wd * hidden[i] * 2) * e
-        bias[i] -= db * e
-
-plt.plot(range(len(j)), j) ; plt.show()
-o = [X[:5]]
-for i in range(d - 1):
-    o.append(o[-1] @ hidden[i] + bias[i])
-    o[-1] = relu(o[-1]) * o[-1]
-o.append(softmax(o[-1] @ hidden[d - 1]))
-print (o[-1] - get_one_hot(y[0], 2))
+j = nn.rmsprop(100, x, y, e=1e-2, wd=0)
+fig, axs = plt.subplots(2)
+axs[0].plot(range(len(j)), j)
+plt.show()
