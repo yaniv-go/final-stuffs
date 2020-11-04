b5177d924a9c289b9b3ab3f707435e6b838657e8
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-11-04 19:44:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+"""
 a = np.arange(9) + 1
 a = np.array((a, np.roll(a, 1), np.roll(a, 2))).T
 k = 3
@@ -18,15 +19,18 @@ for j in range(a.shape[1]):
         if i == 8: p = 0
 print (a)
 print (x)
-
+"""
 
 a = np.arange(9) + 1
-a = np.array((a, np.roll(a, 1), np.roll(a, 2))).T
-k = 3
-p = 0 ; c = 3
-x = 0
+a = np.array((a, np.roll(a, 1), np.roll(a, 2)))
+k = 3 ; c = 0
+i = 0 ; j = 1
+x = a[i, j]
 
-for j in range(a.shape[1]):
-    for i in range(a.shape[0]):
-        p = i // k
-        
+while c != 26 - 1:
+    p = j // k
+    m = j - 3 * p
+    n = i * 3 + p
+    a[m, n], x = x, a[m, n]
+    i = m ; j = n
+    print (a)
