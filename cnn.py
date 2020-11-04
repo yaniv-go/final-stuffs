df9eeab1561b64cfcf9acb1038ad63e5a43805f4
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-11-04 14:08:22 +0200

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -4,9 +4,49 @@ import matplotlib.pyplot as plt
 import sklearn.datasets as sk
 import random
 
+class ConvLayer:
+    def __init__(self, size=3, amount=2,  pad=1, stride=1):
+        self.ks = size ; self.p = pad ; self.s = stride
+        self.kernels = np.array([np.random.rand(self.ks, self.ks) * np.sqrt(2. / self.ks) for i in range(amount)])
+        
+
 class CNN:
     def __init__(self):
         self.nn = {}
-        self.layers = []
         depth = 0
-    
+        
+    def get_im2col_indices(x_shape, field_height, field_width, padding=1, stride=1):
+        # First figure out what the size of the output should be
+        N, C, H, W = x_shape
+        assert (H + 2 * padding - field_height) % stride == 0
+        assert (W + 2 * padding - field_height) % stride == 0
+        out_height = (H + 2 * padding - field_height) / stride + 1
+        out_width = (W + 2 * padding - field_width) / stride + 1
+
+        i0 = np.repeat(np.arange(field_height), field_width)
+        i0 = np.tile(i0, C)
+        i1 = stride * np.repeat(np.arange(out_height), out_width)
+        j0 = np.tile(np.arange(field_width), field_height * C)
+        j1 = stride * np.tile(np.arange(out_width), out_height)
+        i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+        j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+
+        k = np.repeat(np.arange(C), field_height * field_width).reshape(-1, 1)
+
+        return (k, i, j)
+
+    def im2col_indices(x, field_height, field_width, padding=1, stride=1):
+        """ An implementation of im2col based on some fancy indexing """
+        # Zero-pad the input
+        p = padding
+        x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
+
+        k, i, j = get_im2col_indices(x.shape, field_height, field_width, padding,
+                                    stride)
+
+        cols = x_padded[:, k, i, j]
+        C = x.shape[1]
+        cols = cols.transpose(1, 2, 0).reshape(field_height * field_width * C, -1)
+        return cols
+
+c = ConvLayer()
