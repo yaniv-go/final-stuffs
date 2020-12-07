91963fc3e46feffe56f35be55f557a548bd55553
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2020-12-08 00:20:29 +0200

    asd
    asd

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def normal(a, b):
     a + b
 
 def numpy(a, b):
-    np.add(a, b)
+    np.dot(a, b)
 
 def cupy(a, b):
     cp.add(a, b)
@@ -14,8 +14,14 @@ def cupy(a, b):
 
 a = np.arange(1e7).reshape((100, -1))
 b = a + 37
+b = b
 
 ca = cp.array(a)
 cb = cp.array(b)
 
-print(timeit.timeit('normal(a, b)', globals=globals()))
+
+normal(ca, cb)
+cupy(ca, cb)
+
+print(timeit.timeit('normal(ca, cb)', globals=globals(), number=10000))
+print(timeit.timeit('cupy(ca, cb)', globals=globals(), number=10000)) 
