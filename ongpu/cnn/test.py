b8f73add47ce18f18aa68ff878638e819de6fb75
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2021-01-02 15:30:12 +0200

    asd
    asd

--- ongpu/cnn/test.py
+++ ongpu/cnn/test.py
@@ -3,3 +3,13 @@ import cupy as cp
 import numpy as np
 from layers import *
 
+a = cp.arange(4*4*3*2).reshape(3,2,4,4)
+print (a)
+
+print ('and now the transposed: \n')
+a = a.transpose(1, 0, 2, 3)
+print (a)
+
+print('and now the vectorized: \n')
+a = a.reshape(a.shape[0], a.shape[1], -1)
+print(a)
