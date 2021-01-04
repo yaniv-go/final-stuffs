8fa11681156b4417b6d8041bd89d78b87f07fc40
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2021-01-04 22:30:26 +0200

    asd
    asd

--- ongpu/cnn/test.py
+++ ongpu/cnn/test.py
@@ -4,15 +4,14 @@ import numpy as np
 from layers import *
 from PIL import Image
 import pandas as pd
-
+import os
 
 dataset_path = 'C:\\Users\\yaniv\\Documents\\datasets\\dog-breed-identification'
 
 
 csv = pd.read_csv(dataset_path + '\\labels.csv')
 
-for filename in csv['id']:
-    print (filename)
-    image = Image.open(dataset_path + '\\train\\' + filename + '.jpg')
+for filename in os.listdir(dataset_path + '\\test'):
+    image = Image.open(dataset_path + '\\test\\' + filename)
     new_image = image.resize((224, 224))
-    new_image.save(dataset_path + '\\edited-train\\' + filename + '.jpg')
+    new_image.save(dataset_path + '\\edited-test\\' + filename)
