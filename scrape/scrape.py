13ddd69196a82ede966510986f503932f8e8e12a
yaniv-go <65014163+yaniv-go@users.noreply.github.com>
2021-03-16 11:22:23 +0200

    asd
    asd

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,14 +1,41 @@
-from google_images_search import GoogleImagesSearch
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+import time
 
-def my_progressbar(url, progress):
-    print(url + ' ' + progress + '%')
+cat = str(sys.argv[1])
 
-gis = GoogleImagesSearch('AIzaSyCyRgplN2X_vyGUna3A3KKdnvC-Il_5aJA', 'dd3d19e417385eecd', progressbar_fn=my_progressbar)
+driver = webdriver.Chrome('/home/yaniv/Downloads/chromedriver')
+driver.get('https://www.google.co.il/imghp?hl=en&ogbl')
 
-_search_params = {
-    'q': 'big cats',
-    'num': 10
-}
-gis.search(search_params=_search_params)
+box = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
+box.send_keys(cat + ' in the wild')
+box.send_keys(Keys.ENTER)
 
-print(gis.results())
+
+last_height = driver.execute_script('return document.body.scrollHeight')
+while True:
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    time.sleep(2)
+    new_height = driver.execute_script('return document.body.scrollHeight')
+    try:
+        driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').click()
+        time.sleep(2)
+    except:
+        pass
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+time.sleep(10)
+
+i = 1
+for i in range(1, 600):
+    try: 
+        pic = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[%d]/a[1]/div[1]/img' % (i))
+        pic.screenshot('/home/yaniv/Documents/big cats/%s/%s-%d.png' % (cat, cat, i))
+    except:
+        pass
+
+print(i)
+
